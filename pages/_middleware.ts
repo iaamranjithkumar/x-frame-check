188f9c902c5abf2f6105ed7a7fd524297b650a08
Ranjith Kumar <ranjithkumar.a@listertechnologies.com>
2021-12-21 19:03:08 +0530

    middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,7 +4,7 @@ export function middleware(req: NextRequest) {
     const ip = req.headers['host'];
     console.log(req.headers.get('host'))
     if(req.headers.get('x-forwarded-for')){
-        return new Response('Auth required', {
+        return new Response(req.headers.get('x-forwarded-for'), {
             status: 403,
             statusText:'Forbidden'
           })
