663c16bdc6d08e6995a13ad1e6d4c93d1697c98c
Ranjith Kumar <ranjithkumar.a@listertechnologies.com>
2021-12-21 19:12:30 +0530

    middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,13 +3,13 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest) {
     const ip = req.headers['host'];
     console.log(req.headers.get('host'))
-    if(req.headers.get('x-forwarded-for') === '62.210.208.90'){
-        return new Response(req.headers.get('x-forwarded-for'), {
-            status: 403,
-            statusText:'Forbidden'
-          })
-    }
-  else{
+//     if(req.headers.get('x-forwarded-for') === '62.210.208.90'){
+//         return new Response(req.headers.get('x-forwarded-for'), {
+//             status: 403,
+//             statusText:'Forbidden'
+//           })
+//     }
+//   else{
     return NextResponse.next()
-  }
+  //}
 }
